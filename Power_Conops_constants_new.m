7cff80637ffd76b193d2db70376a7d4e789ba7b5
Sebastian Bernal <sbernal2018@gmail.com>
2020-08-05 20:46:07 -0400

    made sure to incorporate energy consumption of all avionics, not just the obstacle avoidance higher resolution achieved; now tracking power consumption on a minute basis rock avoidance causes rapid decline in energy as noted in graphs need to resolve an index issue toward end of vectors (distance and SOC)

---
+++ Power_Conops_constants_new.m
@@ -0,0 +1,96 @@
+%to be modified down the road
+
+%time_scale will change between 1 and 60 depending
+%on the user-input in main_new
+time_scale = 60;
+time_start = 0;
+time_step  = 1;   %change between .25 and 1, .25 hours, 1 min
+time_end   = 18*time_scale; %18 should be replaced by
+                            %rove_duration in main_new
+
+time_vector = time_start: time_step: time_end;
+tv_length = length(time_vector);
+
+%relevant modes where (1) load_out in Watts, (2) load_in in Watts
+%includes 30 percent power growth
+rove_link   = [50, 24];
+charge_link = [32, 48];
+nom_rove    = [50, 24];
+charge_min  = [29, 48];  %update this solar in
+                            %with new power calculations
+                            
+plan_trek_interval = [0: time_step: 1*time_scale];
+downlink_interval  = [1: time_step: 2*time_scale];
+trek_phase1        = [plan_trek_interval, downlink_interval];
+                                                               
+%Battery Characteristics at Deployment
+battery_total = 200;
+init_soc      = .40;
+
+%rover speed
+speed_centi  = 2;
+speed_reg    = 7.2;
+
+load_out           = zeros(1,tv_length);
+load_in            = zeros(1,tv_length);
+battery_soc        = zeros(1,tv_length);
+net_power          = zeros(1,tv_length);
+battery_cap        = zeros(1,tv_length);
+solar_incidence    = zeros(1,tv_length); %in radians
+distance_travelled = zeros(1,tv_length);
+
+
+%populating solar_incidence first since
+%the load_in vector values are dependent on angle
+index = 1;
+for spec_time = time_vector
+     if (index > 1)
+        prev_value = solar_incidence(index-1);
+        divide_factor = time_step*time_scale;
+        %for every .25 hours, or 1/4 hours, divide_factor was 4
+        diff = (360/(29.5*24)) / divide_factor; %previously, calculation was for .25 hours
+        solar_incidence(index) = prev_value + diff;
+     end
+     index = index + 1;
+end
+
+
+
+index = 1;
+%this is the first two hours where we have downlink
+for spec_time = trek_phase1
+    %solar angle in degrees, but must be in rad for MATLAB
+    curr_sangle_offset = cos(deg2rad(solar_incidence(index)));
+    
+    if (ismember(spec_time, plan_trek_interval))
+        load_out(index) = charge_min(1);
+        load_in(index)  = charge_min(2)*curr_sangle_offset;
+    elseif (ismember(spec_time, downlink_interval))
+        load_out(index) = charge_link(1);
+        load_in(index)  = charge_link(2)*curr_sangle_offset;
+    end
+    
+    net_power(index) = load_in(index) - load_out(index);
+
+    if (index > 1)
+        energy_change = (time_step*net_power(index)) / time_scale;
+        battery_cap(index) = battery_cap(index-1) + energy_change;
+        battery_soc(index) = battery_cap(index)/battery_total;    
+    else
+        battery_cap(1) = battery_total*init_soc;
+        battery_soc(1) = battery_cap(1)/battery_total;
+    end
+    
+    index = index + 1;
+end
+
+meters_per_sec = speed_centi/100;
+distance_covered = meters_per_sec * time_step * time_scale;
+
+
+
+
+
+
+
+
--- Power_Conops_main_new.m
+++ Power_Conops_main_new.m
@@ -6,52 +6,78 @@
 
 begin_charge_soc = .5;
 end_charge_soc   = .6;
+rove_duration    =  0; %in hours
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%% END USER DEFINED PARAMETERS
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %26 insurmountable rocks according to rock distribution data 
-rock_findings = [3, 5, 6.5, 9, 11.5, 14, 15.5, ...
-                 16.5, 22, 24, 26.5, 28.75, ...
-                 31, 33.75, 35, 37, 38.5, 40 ...
-                 41.5, 43, 45.75, 47, 48.5, ...
-                 50, 51];
+%need to change this to 40 rocks
+rock_findings = [150,153,156,159,165 ...
+                 250, 256, 280];
 
 is_charging = false;
-time_charging = 0;
-trek_phase2 = [2.25 : .25: 17.75];
+is_avoiding = false;
+time_avoiding = 0; %in mins
+trek_phase2 = [2*time_scale + time_step : time_step: time_end - 1];
+
+%we broke up the avoidance manuevers into 2 parts,
+%one part consists of energy expended by 2 90-degree point turns
+%the second part consists of energy expended while skid-steering
+
+%don't forget to also consider the energy used by avionics
+%energy used by avionics during avoidance = power * 3mins * 60s/1min
+%must convert it to Wh, so divide by 3600
+avoidance_duration = 3; %in mins
+point_turn_energy = 10 * 28; %power*time for 90-degree turn
+skid_energy = 1.2 * point_turn_energy; %according to papers we read
+
+%next two calculations need to incorporate time_scale somehow
+avionics_consumption = (45.01 * 1 * 60)/3600; %power * time in mins * 60s 
+avoidance_consumption = (2*point_turn_energy + skid_energy)/(3600*avoidance_duration); %divide by 60 for per-minute consumption
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %after initial two hours of charging, battery SOC should be at around 90%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %starting at index 11, since this is where contants file left off
-index = 11;
+index = 121;
 for spec_time = trek_phase2
+    sprintf("Current time and index: %d, %d", spec_time, index)
     rock_found = ismember(spec_time, rock_findings);
     curr_sangle_offset = cos(deg2rad(solar_incidence(index)));
     
     if (rock_found && ~is_charging)
         display("rock was found")         
-        %we broke up the avoidance manuevers into 2 parts,
-        %one part consists of energy expended by 2 90-degree point turns
-        %the second part consists of energy expended while skid-steering
+        is_avoiding = true;
         
-        %don't forget to also consider the energy used by avionics
-        %energy used by avionics during avoidance = power * 3mins * 60s/1min
-        %must convert it to Wh, so divide by 3600
-        
-        avoidance_duration = 3; %in mins
-        point_turn_energy = 10 * 28; %power*time for 90-degree turn
-        skid_energy = 1.2 * point_turn_energy; %according to papers we read
-        avionics_consumption = (45.01 * 3 * 60)/3600;
-        avoidance_consumption = (2*point_turn_energy + skid_energy)/3600;
         battery_cap(index) = battery_cap(index-1) ...
                              - avoidance_consumption - avionics_consumption;
         battery_soc(index) = battery_cap(index)/battery_total;
         distance_travelled(index) = distance_travelled(index-1)+distance_covered;
         index = index + 1;
         continue
+    elseif (is_avoiding)
+        if (time_avoiding == avoidance_duration)
+            is_avoiding = false;
+            time_avoiding = 0;
+            
+            energy_change = (time_step*net_power(index)) / time_scale;
+            battery_cap(index) = battery_cap(index-1) + energy_change;
+            battery_soc(index) = battery_cap(index)/battery_total;
+            distance_travelled(index) = distance_travelled(index-1)+distance_covered;
+            index = index + 1;
+            continue
+        else
+            time_avoiding = time_avoiding + 1;
+            
+            battery_cap(index) = battery_cap(index-1) ...
+                                 - avoidance_consumption - avionics_consumption;
+            battery_soc(index) = battery_cap(index)/battery_total;
+            distance_travelled(index) = distance_travelled(index-1)+distance_covered;
+            index = index + 1;
+            continue
+        end
     end
 
     if (battery_soc(index-1) < begin_charge_soc && ~is_charging)
@@ -60,20 +86,21 @@ for spec_time = trek_phase2
         load_out(index) = charge_min(1);
         load_in(index)  = charge_min(2)*curr_sangle_offset;
         net_power(index) = load_in(index) - load_out(index);
-        battery_cap(index) = battery_cap(index-1) + time_step*net_power(index);
+        
+        energy_change = (time_step*net_power(index)) / time_scale;
+        battery_cap(index) = battery_cap(index-1) + energy_change;
         battery_soc(index) = battery_cap(index)/battery_total;
         distance_travelled(index) = distance_travelled(index-1);
         display("STARTING TO CHARGE")
     elseif (is_charging)
         if (battery_soc(index-1) >= end_charge_soc)
             
-            display("DONE CHARGING")
-            time_charging = 0;
-            
+            display("DONE CHARGING")          
             load_out(index) = nom_rove(1);
             load_in(index)  = nom_rove(2);
             net_power(index) = load_in(index) - load_out(index);
-            battery_cap(index) = battery_cap(index-1) + time_step*net_power(index);
+            energy_change = (time_step*net_power(index)) / time_scale;
+            battery_cap(index) = battery_cap(index-1) + energy_change;
             battery_soc(index) = battery_cap(index)/battery_total;
             distance_travelled(index) = distance_travelled(index-1)+distance_covered;
             
@@ -81,12 +108,11 @@ for spec_time = trek_phase2
             index = index + 1;
             continue
         else
-            time_charging = time_charging + .25;
-            
             load_out(index) = charge_min(1);
             load_in(index)  = charge_min(2)*curr_sangle_offset;
             net_power(index) = load_in(index) - load_out(index);
-            battery_cap(index) = battery_cap(index-1) + time_step*net_power(index);
+            energy_change = (time_step*net_power(index)) / time_scale;
+            battery_cap(index) = battery_cap(index-1) + energy_change;
             battery_soc(index) = battery_cap(index)/battery_total;
             distance_travelled(index) = distance_travelled(index-1);
             
@@ -95,7 +121,8 @@ for spec_time = trek_phase2
         load_out(index) = nom_rove(1);
         load_in(index)  = nom_rove(2);
         net_power(index) = load_in(index) - load_out(index);
-        battery_cap(index) = battery_cap(index-1) + time_step*net_power(index);
+        energy_change = (time_step*net_power(index)) / time_scale;
+        battery_cap(index) = battery_cap(index-1) + energy_change;
         battery_soc(index) = battery_cap(index)/battery_total;
         distance_travelled(index) = distance_travelled(index-1)+distance_covered;
     end
